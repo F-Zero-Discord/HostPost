811d536db067a1dda43b1bd8e0d6a5b0d1f11cc7
lurch-deliberately <lurch.mclurchington@gmail.com>
2026-08-08 21:56:05 -0700

    Commented out anchor post command after successful completion.

--- src/cogs/hosting_signup.py
+++ src/cogs/hosting_signup.py
@@ -193,24 +193,24 @@ class HostingSchedule(commands.Cog):
             await interaction.response.send_message("An error occurred while fetching the hosting schedule.", ephemeral=True)
 
 
-    @app_commands.command(name="anchor_post", description="create anchor post")
-    @discord.app_commands.checks.has_any_role(*access_roles)
-    async def anchor_post(self, interaction: discord.Interaction):
-        """ Temporary command to create a message for the bot to update the schedule in.
-        """
-        async with get_db_connection(self.bot.db_pool) as db:
-            event_dict = await get_hosting_schedule(db)
-        schedule_board = HostingSchedule.build_schedule_embed(event_dict=event_dict)
-        post_channel = self.bot.get_channel(
-            discord.Object(id=int(get_settings().hosting_schedule_channel)).id)
-        await post_channel.send(embed=schedule_board)
-        await interaction.response.send_message("Anchor message sent.")
+    # @app_commands.command(name="anchor_post", description="create anchor post")
+    # @discord.app_commands.checks.has_any_role(*access_roles)
+    # async def anchor_post(self, interaction: discord.Interaction):
+    #     """ Temporary command to create a message for the bot to update the schedule in.
+    #     """
+    #     async with get_db_connection(self.bot.db_pool) as db:
+    #         event_dict = await get_hosting_schedule(db)
+    #     schedule_board = HostingSchedule.build_schedule_embed(event_dict=event_dict)
+    #     post_channel = self.bot.get_channel(
+    #         discord.Object(id=int(get_settings().hosting_schedule_channel)).id)
+    #     await post_channel.send(embed=schedule_board)
+    #     await interaction.response.send_message("Anchor message sent.")
             
 
     @hosting_schedule.error
     @update_host_for_event.error
     @remove_host_from_event.error
-    @anchor_post.error
+    # @anchor_post.error
     async def role_error(self, interaction: discord.Interaction, error):
         if isinstance(error, app_commands.MissingAnyRole):
             await interaction.response.send_message(
