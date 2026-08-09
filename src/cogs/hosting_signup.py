31b3c04f595667f3f198a125c9131aca03e8c3a7
lurch-deliberately <lurch.mclurchington@gmail.com>
2026-08-08 21:41:30 -0700

    Troubleshooting sending messages to private channels.

--- src/cogs/hosting_signup.py
+++ src/cogs/hosting_signup.py
@@ -81,11 +81,11 @@ class HostingSchedule(commands.Cog):
             await message.edit(embed=schedule_board)
 
         except discord.NotFound:
-            await interaction.send("Error: The message or channel could not be found.")
+            await interaction.response.send_messsage("Error: The message or channel could not be found.")
         except discord.Forbidden:
-            await interaction.send("Error: The bot does not have permissions to edit or view this.")
+            await interaction.response.send_messsage("Error: The bot does not have permissions to edit or view this.")
         except discord.HTTPException as e:
-            await interaction.send(f"An error occurred: {e}")
+            await interaction.response.send_messsage(f"An error occurred: {e}")
 
     
     async def get_or_create_db_user(self, db, discord_user):
